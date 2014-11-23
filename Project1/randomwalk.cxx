7855ded597a06eb814b23eaa218f1b47f8ec4979
Jordan Weiler <jwei7er@users.noreply.github.com>
2014-11-23 00:43:00 -0800

    Update documentation

--- Project1/randomwalk.cxx
+++ Project1/randomwalk.cxx
@@ -1,22 +1,25 @@
 /*==============================================================================
- 
+
  Author: Jordan Weiler
  Date:   April 24, 2014
- 
+
  This program simulates the behavior of many random walks on a large graph.
  It takes an input file consisting of positive integer edges of an undirected
- graph, an output file name, and the number of times to simulate. Each node 
- starts with a credit of 1.0. For each subsequent round of simulation, the
- node's credit is determined by the credit of each neighbor divided by each
- neighbor's degree.
- 
+ graph, an output file name, and the number of times to simulate. The output
+ file contains the nodes, the number of degrees for each node, and each node's
+ credit for each round in the simulation.
+
+ Each node starts with a credit of 1.0. For each subsequent round of
+ simulation, the node's credit is determined by the credit of each neighbor
+ divided by each neighbor's degree.
+
  For example, if node i has four neighbors with credit values of 0.1, 0.23,
  0.35, and 0.64 and node degrees of 5, 27, 19, and 8 respectively, the credit
  for node i after the round would be:
- 
+
  c(t+1, i) = (0.1/5) + (0.23/27) + (0.35/19) + (0.64/8)
            = 0.126940
- 
+
 ==============================================================================*/
 
 #include <iostream>
